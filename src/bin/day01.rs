63b486ad60d9e244769d835648b9b2e482056ef5
energy-in-joles <nzmjoel@gmail.com>
2025-12-11 19:19:14 +0000

    working solution for day 1

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,14 +1,15 @@
-use std::io;
-use aoc25_rust::read_input_lines;
 use aoc25_rust::cli_parse;
+use aoc25_rust::read_input_lines;
+use aoc25_rust::AoCArgs;
+use std::io;
 
 const START_POSITION: i32 = 50;
 const DIAL_RANGE: i32 = 100;
 
 fn parse_movement(s: &str) -> Option<i32> {
     let mut chars = s.chars();
-    let first = chars.next()?;        // first character
-    let rest = chars.as_str();        // remaining string slice
+    let first = chars.next()?; // first character
+    let rest = chars.as_str(); // remaining string slice
     let steps = rest.parse::<i32>().ok()?; // returns None if parsing fails
     if first == 'R' {
         Some(steps)
@@ -21,16 +22,64 @@ fn parse_movement(s: &str) -> Option<i32> {
 
 fn main() -> io::Result<()> {
     let args = cli_parse();
+    match args.p {
+        Some(1) => part1(&args)?,
+        Some(2) => part2(&args)?,
+        None => {
+            part1(&args)?;
+            part2(&args)?;
+        }
+        Some(_) => println!("Invalid part number. Use 1 or 2."),
+    }
+    Ok(())
+}
+
+fn part1(args: &AoCArgs) -> io::Result<()> {
     let mut position = START_POSITION;
     let mut zero_count = 0;
     for line in read_input_lines(args.s)? {
-        let movement = parse_movement(&line)
-            .expect("Failed to parse movement command");
+        let movement = parse_movement(&line).expect("Failed to parse movement command");
         position += movement;
         if position % DIAL_RANGE == 0 {
             zero_count += 1;
         }
     }
-    println!("Times zero position reached: {}", zero_count);
+    println!("Part 1: Times zero position reached: {}", zero_count);
+    Ok(())
+}
+
+fn wrap_position(pos: i32) -> i32 {
+    (pos % DIAL_RANGE + DIAL_RANGE) % DIAL_RANGE
+}
+
+fn count_zero_crossover(current_position: i32, movement: i32) -> i32 {
+    let q = movement / DIAL_RANGE;
+    let r = movement % DIAL_RANGE;
+    if (r != 0 && current_position != 0)
+        && (((current_position + r) >= DIAL_RANGE) || ((current_position + r) <= 0))
+    {
+        return 1 + q.abs();
+    }
+    q.abs()
+}
+
+// fn count_zero_crossover(current: i32, movement: i32) -> i32 {
+//     let start_div = current.div_euclid(DIAL_RANGE);
+//     let end_div = (current + movement).div_euclid(DIAL_RANGE);
+//     (end_div - start_div).abs()
+// }
+
+fn part2(args: &AoCArgs) -> io::Result<()> {
+    let mut position = START_POSITION;
+    let mut crossover_count = 0;
+    for line in read_input_lines(args.s)? {
+        let movement = parse_movement(&line).expect("Failed to parse movement command");
+        crossover_count += count_zero_crossover(position, movement);
+        position = wrap_position(position + movement);
+    }
+    println!(
+        "Part 2: Times zero position was crossed over: {}",
+        crossover_count
+    );
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
+use clap::Parser;
 use std::env;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 use std::path::PathBuf;
-use clap::Parser;
 
 #[derive(Parser, Debug)]
 pub struct AoCArgs {
@@ -20,13 +20,15 @@ pub fn cli_parse() -> AoCArgs {
 }
 
 pub fn read_input_lines(is_sample: bool) -> io::Result<impl Iterator<Item = String>> {
-    let exe_path = env::current_exe()?;                    // temporary lives here
+    let exe_path = env::current_exe()?; // temporary lives here
     let exe_name = exe_path
         .file_stem()
         .and_then(|s| s.to_str())
         .expect("Failed to get exe name");
 
-    let day: u8 = exe_name[3..].parse().expect("Executable name must be like dayXX");
+    let day: u8 = exe_name[3..]
+        .parse()
+        .expect("Executable name must be like dayXX");
 
     let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
     path.push("src");
@@ -36,9 +38,10 @@ pub fn read_input_lines(is_sample: bool) -> io::Result<impl Iterator<Item = Stri
         path.push("inputs");
     }
     path.push(format!("day{:02}.txt", day));
-    println!("Reading input from: {}", path.display());
     let file = File::open(path)?;
     let reader = BufReader::new(file);
 
-    Ok(reader.lines().map(|line| line.expect("Failed to read line")))
+    Ok(reader
+        .lines()
+        .map(|line| line.expect("Failed to read line")))
 }
